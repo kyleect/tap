a4fc0a945d025833ff7b90d7f4b62766504dd401
Kylee Tilley <kyleetilley@gmail.com>
2016-05-24 00:19:43 -0500

    Adding jsdoc comment to Object.prototype.tap

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ declare interface Object {
   tap<T> (fn: (t: T) => void): T
 }
 
+/**
+ * Tap in to a method call chain, call function and continue call chain
+ * @memberof Object
+ * @param {function} fn - Function called
+ * @returns {any} Call chain entry point value
+ */
 Object.prototype.tap = function tap<T> (fn: (t: T) => void): T {
   const clone: T = Object.assign({ __proto__: this.__proto__ }, this);
   fn.call(null, clone);
